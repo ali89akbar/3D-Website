f0c22fe81416508741345dad447845d21672a560
ali89akbar <alia42268@gmail.com>
2025-06-08 03:59:04 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    <h1 className='bg-green-100 p-[20px] text-blue-200 '>hello</h1>
+  </div>
+  )
+}
+
+export default App
