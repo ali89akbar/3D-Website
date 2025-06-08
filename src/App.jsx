b5eb426894721bc3fd4c7ddfc04d541efb974729
ali89akbar <alia42268@gmail.com>
2025-06-08 17:46:53 +0500

    Hero Section with 3D Spline Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import React from 'react';
 import './App.css'
 import Header from './Components/Header/Header';
+import HeroSection from './Components/Hero/HeroSection';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <Header/>
-
+    <HeroSection/>
+    </>
   )
 }
 
